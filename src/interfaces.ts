9e7e1bb07026f95780c9dd3c8ac65d707c7330a1
Alex Welsh <alex.n.welsh@gmail.com>
2018-04-11 18:12:07 -0700

    interfaces for returned json

---
+++ src/interfaces.ts
@@ -0,0 +1,46 @@
+export interface PostModel {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export interface CommentModel {
+	postId: number,
+	id:number,
+	name: string,
+	email: string,
+	body: string
+}
+
+export interface PhotoModel {
+	albumId: number,
+	id:number,
+	title: string,
+	url: string,
+	thumbnailUrl: string
+}
+
+export interface UserModel {
+	id: number,
+	name: string,
+	username: string,
+	email: string,
+	address:{
+		street: string,
+		suite: string,
+		city: string,
+		zipcode: string,
+		geo:{
+			lat: string,
+			lng: string
+		}
+	},
+	phone: string,
+	website: string,
+	company: {
+		name: string,
+		catchPhrase: string,
+		bs: string
+	}
+}
